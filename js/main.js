d5c45a5922d1df8f817881677f301a4567e3e6aa
kostfeld <kostfeld@potomacfusion.com>
2012-11-29 22:15:47 -0600

    Many updates. Integrated team mate work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+var sectionMappings = {
+    "physiology": "physiology_content",
+    "pathophysiology": "pathophysiology_content",
+    "relevance": "relevance_content",
+    "treatment": "treatment_content",
+    "developments": "developments_content"
+};
 
-function navigate(newDivId) {
+function navigate(newSection, skipAnchorSet) {
+    var newDivId = sectionMappings[newSection];
+
     var navigatable = document.getElementById('navigatable');
     var newDiv = document.getElementById(newDivId);
 
@@ -12,4 +21,18 @@ function navigate(newDivId) {
     }
 
     newDiv.style.display = "block";
+
+    if (skipAnchorSet !== true) {
+        parent.location.hash = newSection;
+    }
+}
+
+function pageLoad() {
+    var target = parent.location.hash;
+    target = target.trim();
+    target = target.replace(/^#/, "");
+    if (target === null || target === "") {
+        target = "physiology";
+    }
+    navigate(target, true);
 }
